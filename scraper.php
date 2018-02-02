fe587ee443820c2e5f17ea886d1b1c8336802ffe
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-02 16:11:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-$years	=	array('1972',
+$years	=	array(/*'1972',
 '1973',
 '1974',
 '1974',
@@ -32,7 +32,7 @@ $years	=	array('1972',
 '1998',
 '1999',
 '2000',
-'2001',
+'2001',*/
 '2002',
 '2003',
 '2004',
@@ -59,7 +59,7 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
   $link   = 'http://202.61.43.40:8056/caselaw/rpt_search_simple.php?CASENO=&CASEYEAR='.$Mainpage.'&STD_CASETYPES=-1&STD_BENCHTYPES=-1&STD_COURTS=-1'; 
   echo "$link\n";
   $page   = file_get_html($link);
-	sleep(60);
+	sleep(1000);
   foreach($page->find("//[@id='tblExport']/tbody/tr")as $element)
   {
       $code          	= $element->find('td',0)->plaintext;
