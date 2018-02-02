7fff8a280b7ebdf4f3762b7e6a8bfe9cf1e3327f
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-02 15:02:55 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -69,11 +69,16 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
       $order_date   	= $element->find('td',7)->plaintext;
       $afr          	= $element->find('td',8)->plaintext;    
       $head  		= $element->find('td',9)->plaintext;     
-      $judlink   	= $element->find('td/a',0)->href;  
-      $jud_order	=	'http://202.61.43.40:8056/caselaw/'."$judlink\n";
+     
+      $judlinkone  	= $element->find('td/a',0)->href;  
+      $jud_ordertwo  	= $element->find('td/a',1)->href;
+	  
+      $jud_orderek	=	'http://202.61.43.40:8056/caselaw/'."$judlink\n";
+      $jud_orderdo	=	'http://202.61.43.40:8056/caselaw/'."$jud_ordertwo\n";
+      
     if($code != null || $code != "")
     {
-    echo "$jud_order\n";
+    echo "$jud_orderdo\n";
     $record = array( 'code' =>$code, 
 		   's' => $s,
 		   'citation' => $citation, 
